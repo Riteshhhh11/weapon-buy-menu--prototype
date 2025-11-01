37e839728be5ab2f0226e1a50dd96aface7a29b8
Riteshhhh <riteshranjan785@gmail.com>
2025-11-01 14:53:45 +0000

    Update Buy menu UI and spawning gun prefab

--- Assets/Scripts/BuyMenuUI.cs
+++ Assets/Scripts/BuyMenuUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using TMPro;
+using System.Collections.Generic;
 
 public class BuyMenuUI : MonoBehaviour
 {
@@ -10,15 +11,18 @@ public class BuyMenuUI : MonoBehaviour
     public TextMeshProUGUI weaponName;
     public TextMeshProUGUI weaponCategory;
     public TextMeshProUGUI weaponPrice;
+    public TextMeshProUGUI magzineRounds;
+    public Transform GunHolderTransform;
     //public TextMeshProUGUI weaponRoundCapacity;
 
-    public ScriptableGuns ScriptableGuns;
+    //public ScriptableGuns SO;
+
     void Start()
     {
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
 
         if (buyMenuPanel == null) {
-            Debug.LogError("BuyMenuPanel not found in the scene.");
+            //Debug.LogError("BuyMenuPanel not found in the scene.");
         }
         buyMenuPanel.SetActive(isBuyMenuOpen);
     }
@@ -38,30 +42,34 @@ public class BuyMenuUI : MonoBehaviour
         if (UnityEngine.Cursor.lockState == CursorLockMode.Locked) {
 
             UnityEngine.Cursor.lockState = CursorLockMode.None;
-            Debug.Log("Cursor unlocked");
+            //Debug.Log("Cursor unlocked");
         }
         else 
         { 
             UnityEngine.Cursor.lockState = CursorLockMode.Locked; 
-            Debug.Log("Cursor locked");
+            //Debug.Log("Cursor locked");
         }
         if (isBuyMenuOpen == true && UnityEngine.Cursor.lockState == CursorLockMode.Locked)
         {
             buyMenuPanel.SetActive(true);
         } 
     }
-    public void ShowWeaponStats() {
-        weaponName.SetText(ScriptableGuns.gunName);
-        Debug.Log("Showing stats for: " + ScriptableGuns.gunName);
-        weaponCategory.SetText(ScriptableGuns.gunCategory);
-        weaponPrice.SetText(ScriptableGuns.gunPrice.ToString());
-        //weaponRoundCapacity.SetText(ScriptableGuns.roundCapacity.ToString());
+    public void ShowWeaponStats(ScriptableGuns SO) {
+         weaponName.SetText(SO.gunName);
+         weaponCategory.SetText(SO.gunCategory);
+         weaponPrice.SetText(SO.gunPrice.ToString());
+         magzineRounds.SetText(SO.roundCapacity.ToString());
     }
 
     public void ClearWeaponStats() {
          weaponName.text = ("");
          weaponCategory.text = ("");
          weaponPrice.text = ("");
-         //weaponRoundCapacity.text = ("");
+        magzineRounds.text = ("");
+    }
+
+    public void SpawnGun(ScriptableGuns SO) {
+        Instantiate(SO.gunPrefab, GunHolderTransform);
+        Debug.Log("Spawned " + SO.gunName);
     }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,22 +3,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform playerTransform;
-    public float sensitivity;
+    public float sensitivity = 0;
     public float RotationX = 0;
-
-    void Start()
-    {
-
-    }
+    public BuyMenuUI buyMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-        RotationX -= mouseY;
-        RotationX = Mathf.Clamp(RotationX, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(RotationX, 0, 0);
-        playerTransform.Rotate(Vector3.up * mouseX);
-    }
+        if (buyMenuUI.canControlPlayer == false)
+        {
+            return;
+        }
+        else {
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+            RotationX -= mouseY;
+            RotationX = Mathf.Clamp(RotationX, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(RotationX, 0, 0);
+            playerTransform.Rotate(Vector3.up * mouseX);
+            Debug.Log(sensitivity);
+        }
+    }  
 }
